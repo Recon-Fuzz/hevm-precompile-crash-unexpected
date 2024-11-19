130d193b522bfbcd62fe898b8a937061e4b6b307
gallo <alex@entreprenerd.xyz>
2024-11-19 19:50:56 +0700

    feat: third call (doesn't revert)

--- src/Basic.sol
+++ src/Basic.sol
@@ -8,13 +8,20 @@ interface IERC20 {
 contract Basic {
 
     // We expect this to cause a crash as we're rawdogging the precompile
-    // function doACall() public {
-    //     address(0x0a).call("");
-    // }
+    function doACall() public {
+        address(0x0a).call("");
+    }
 
     // We don't expect this to cause a crash because the compiler should prevent us from calling it
+    // but it crashes HEVM
     function doACall() public {
         uint256 res = IERC20(address(0x0a)).balanceOf(address(this));
         res + 1;
+    }
+
+    // Safe, we can actually get the code size
+    function doACall() public {
+        uint256 size = address(0x0a).code.length;
+        size + 1;
     }
 }
